029e26383b722a633cef92809c0e554c1b69a1c5
kangpengjiyep <thxuxy@163.com>
2022-01-15 19:54:37 +0800

    Initial commit

---
+++ src/main/java/com/mooc/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.mooc;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
+@MapperScan("com.mooc.mapper")
+
+public class DemoApplication extends SpringBootServletInitializer{
+	public static void main(String[] args) {
+
+	    SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
+
+}
